6e330814aacead62ecb5b75247b3d475a093e7e8
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-06-19 09:28:49 +0300

    Fix pacman position update and drawing logic in main.lua

--- src/tut/love2d/main.lua
+++ src/tut/love2d/main.lua
@@ -2,14 +2,19 @@ _G.love = require("love")
 
 function love.load()
     love.graphics.setBackgroundColor(0.9, 0.9, 1)
+
+    _G.pacman = {}
+    pacman.x = 200
+    pacman.y = 300
+
 end
 
 function love.update(dt)
-
+    pacman.x =+ 10
 end
 
 function love.draw()
     love.graphics.setColor(1, 0, 0)
     love.graphics.rectangle("fill", 50, 50, 50, 50)
-    love.graphics.circle("line", 300, 300, 50)
+    love.graphics.circle("line", pacman.x, pacman.y, 15)
 end
