888fb8a67a01d60254996021716d1c9d19d77c8d
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-06-19 10:02:44 +0300

    Refactor pacman initialization and update logic in main.lua

--- src/tut/love2d/main.lua
+++ src/tut/love2d/main.lua
@@ -3,18 +3,34 @@ _G.love = require("love")
 function love.load()
     love.graphics.setBackgroundColor(0.9, 0.9, 1)
 
-    _G.pacman = {}
-    pacman.x = 200
-    pacman.y = 300
+    _G.pacman = {
+        x = 200,
+        y = 300,
+        eat = false,
+        food_x = 300
+    }
+    
+    -- pacman.x = 200
+    -- pacman.y = 300
 
 end
 
 function love.update(dt)
-    pacman.x =+ 10
+    pacman.x = pacman.x + 1
+
+    if pacman.x >= pacman.food_x + 20 then 
+        pacman.eat = true
+    end
+    love.graphics.print(pacman.x)
 end
 
 function love.draw()
-    love.graphics.setColor(1, 0, 0)
-    love.graphics.rectangle("fill", 50, 50, 50, 50)
-    love.graphics.circle("line", pacman.x, pacman.y, 15)
+
+    if not pacman.eat then 
+        love.graphics.setColor(1, 0, 0)
+        love.graphics.rectangle("fill", 700, 300, 50, 50)
+    end
+    
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.circle("line", pacman.x, pacman.y, 30)
 end
