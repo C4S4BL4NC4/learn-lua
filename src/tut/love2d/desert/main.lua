9edfa05571f1958e66808c2edee9bc2f7deaf929
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-06-19 13:44:35 +0300

    Initialize character with sprite properties in love.load function

--- src/tut/love2d/desert/main.lua
+++ src/tut/love2d/desert/main.lua
@@ -1,7 +1,17 @@
 _G.love = require("love")
 
 function love.load()
-    
+    _G.character = {
+        -- 832x3456 idle
+        x = 0,
+        y = 0,
+        sprite = {
+            image = love.graphics.newImage("assets/spritesheets/idle-spritesheet.png"),
+            width = 832,
+            height = 3456,
+            quad = 
+        }
+    }
 end
 
 function love.update(dt)
