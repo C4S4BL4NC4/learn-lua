8e4f9a82233ede97535877ae37b15bf29b0d72fb
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-06-19 04:34:55 +0300

    Enhance main.lua with additional control structures and table examples

--- src/tut/helloworld/main.lua
+++ src/tut/helloworld/main.lua
@@ -48,9 +48,11 @@ print(math.floor(2.9))
 print("Random number = "..math.random(0,10))
 
 -- if statements
+print("\nif statements:")
+
 local stat = 2
 
-if x < 5 then
+if stat < 5 then
     print("x is less than 5")
 end
 
@@ -74,3 +76,61 @@ not
 ~=
 59;40
 ]]
+
+-- for loops
+print("\nFor loops:")
+
+for i = 1, 10, 2 do
+    print("Inside of for loop i = "..i)
+end
+
+
+-- while loops
+print("\nWhile loops:")
+
+local cou = 1
+
+while cou < 10 do
+    print("While loop content: i = "..cou)
+    cou = cou + 1
+end
+
+-- repeat loops (do while)
+print("\nRepeat loops:")
+
+local repvar = 10
+
+repeat
+    repvar = repvar + 1
+    print(repvar)
+until repvar > 5
+
+-- user input
+-- io.read()
+-- io.write() -- print() without new line
+
+
+-- tagles (arrays)
+print("\nTables:")
+
+local tbl = {"This", 3, true, 3.99}
+table.insert(tbl, 5)
+table.remove(tbl, 5)
+
+for index, value in pairs(tbl) do
+    print(index, value)
+end
+
+local matrix = {
+    {8, 2, 3},
+    {5, 6, 1},
+    {3, 6, 4}
+}
+
+for i = 1, #matrix do 
+    for j = 1, #matrix[i] do
+        print(matrix[i][j])
+    end
+end
+
+
