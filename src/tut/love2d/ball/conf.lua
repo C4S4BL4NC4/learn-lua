e4dab1774dc030c62411cdd074ceaac702ec0235
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-06-21 19:10:27 +0300

    Add Love2D ball tutorial scaffold and icon
    Introduce initial Love2D 'ball' tutorial files: add conf.lua to configure the window (title, size, position, icon, and window flags), add main.lua with basic love.load/update/draw callbacks and a black background, and add the zombie icon asset at assets/icon/zombie.png. Provides a minimal project scaffold to start development.

---
+++ src/tut/love2d/ball/conf.lua
@@ -0,0 +1,12 @@
+function love.conf(t)
+    t.console = false
+    t.window.title = "Apocalypse: The End is Here"
+    t.window.icon = "assets/icon/zombie.png"
+    t.window.height = 600
+    t.window.width = 800
+    t.window.resizable = false
+    t.window.borderless = false
+    t.window.fullscreen = false
+    t.window.x = 100
+    t.window.y = 100
+end
---
+++ src/tut/love2d/ball/main.lua
@@ -0,0 +1,13 @@
+_G.love = require("love")
+
+function love.load()
+    love.graphics.setBackgroundColor(0, 0, 0)
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    
+end
